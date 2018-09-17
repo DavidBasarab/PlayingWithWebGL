dbf31be12e0f4b22f19910d5a8ddfba8aafab959
David Basarab <dbasarab617@gmail.com>
2018-09-16 21:25:06 -0400

    Got basic squares up and running

---
+++ web-gl-tutorial/src/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {/* TODO */}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    static renderSquare(i) {
+        return <Square />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {Board.renderSquare(0)}
+                    {Board.renderSquare(1)}
+                    {Board.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {Board.renderSquare(3)}
+                    {Board.renderSquare(4)}
+                    {Board.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {Board.renderSquare(6)}
+                    {Board.renderSquare(7)}
+                    {Board.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
